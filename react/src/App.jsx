f02c6a1108bb4ba0a5f144c87eca67b3017fcd01
Xavier Mexious <vinsensiusadrian123@gmail.com>
2023-02-20 22:11:03 +0700

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-purple-500">
+      App.jsx
+      <BeakerIcon className='w-4' />
+    </div>
+  )
+}
+
+export default App
